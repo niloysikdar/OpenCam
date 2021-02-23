e0bd099a8ce0625663a963802bf56784380bd242
Niloy Sikdar <niloysikdar30@gmail.com>
2021-02-23 17:27:16 +0530

    Added basic UI

---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget customAppBar() {
+  return AppBar(
+    title: Text("OpenCam"),
+    centerTitle: true,
+  );
+}
