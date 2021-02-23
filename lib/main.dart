b7f3e75d2a2b5b3a9ddeefb62a230e8d070680b5
Niloy Sikdar <niloysikdar30@gmail.com>
2021-02-23 19:04:58 +0530

    Added Camera and Gallery Picker Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:OpenCam/widgets/appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,6 +16,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  File _image;
+
+  getImage(ImageSource source) async {
+    // ignore: deprecated_member_use
+    var image = await ImagePicker.pickImage(source: source);
+    print(image.lengthSync());
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,55 +34,70 @@ class _MyAppState extends State<MyApp> {
       body: Column(
         children: [
           Container(
-            margin: EdgeInsets.symmetric(horizontal:20.0 , vertical: 20.0),
-            child: Container(
-              height: 300.0,
-              color: Colors.grey[400],
-              child: Center(
-                child: Text(
-                  "Open Camera or\n Pick an image",
-                  style: TextStyle(
-                    fontSize: 20.0,
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            child: (_image == null)
+                ? Container(
+                    height: 300.0,
+                    color: Colors.grey[400],
+                    child: Center(
+                      child: Text(
+                        "Open Camera or\n Pick an image",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  )
+                : Image.file(
+                    _image,
+                    height: 300.0,
                   ),
-                ),
-              ),
-            ),
           ),
           Row(
             children: [
               Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    getImage(ImageSource.camera);
+                  },
                   child: Container(
-                  margin: EdgeInsets.symmetric(horizontal:20.0),
-                  height: 60.0,
-                  decoration: BoxDecoration(
-                    color: Colors.red[400],
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Open Camera",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      color: Colors.red[400],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Open Camera",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
               Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    getImage(ImageSource.gallery);
+                  },
                   child: Container(
-                  margin: EdgeInsets.symmetric(horizontal:20.0),
-                  height: 60.0,
-                  decoration: BoxDecoration(
-                    color: Colors.green[400],
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Open Gallery",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      color: Colors.green[400],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Open Gallery",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
                   ),
